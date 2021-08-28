b75c17d85fbf9c4cd4dde4f3abdbb10008b8c7a7
MonkeyInWind <xlyytcy_pyc@outlook.com>
2021-08-28 21:41:27 +0800

    删除无用代码

--- esp8266/screen.py
+++ esp8266/screen.py
@@ -4,9 +4,6 @@ from ssd1306 import SSD1306_I2C;
 i2c = I2C(scl=Pin(14), sda=Pin(0));
 oled = SSD1306_I2C(128, 64, i2c);
 
-def addText(text, x, y):
-    oled.text(text, x, y);
-
 def refreshDisplay(data):
     oled.fill(0);
     for i in range(len(data)):
